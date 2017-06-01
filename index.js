6700b0f43e81ff59bfb2a15258d3945c57bf2262
coffeefoxx <coffeefox.98@gmail.com>
2017-06-01 09:27:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+﻿ $(function(){
+ 	// check vi tri 
+ 	vitricuawelcome = $('.welcome').offset().top;   // tinh vi tri cua thang div welcome
+ 	 
+// ko tinh vi tri thi cung ko sao, dung luon 750 
+
+
+ 	$(window).scroll(function(event) { // khi cuon chuot thi goi ham 
+// $('body').scrollTop() --> tinh ra vi tri hien tai cua man hinh
+ 		 if($('body').scrollTop() > vitricuawelcome) {  // so sanh vi tri hien tai voi welcome
+ 		 	$('.navbar-brand.logo img').attr('src', 'images/index/logo.png'); // doi logo
+ 		  
+ 		 	 $('.navbar-brand.logo img').addClass('vang'); // them hieu ung 
+ 		 	 $('.menutren').addClass('nentrang');
+ 		 }
+ 		 else   // neu o tren welcome
+ 		 {
+ 		 	$('.navbar-brand.logo img').removeClass('vang');  // ve nhu cu 
+ 		 	$('.menutren').removeClass('nentrang');
+ 		 	$('.navbar-brand.logo img').attr('src', 'images/index/logo2.png');// ve nhu cu 
+ 		 }
+
+ 	});
+})  
+ 
