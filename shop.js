909c0d5a7f1c2dab1372769619ca00f0506b0e02
coffeefoxx <coffeefox.98@gmail.com>
2017-06-02 08:10:07 +0700

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,16 @@
+﻿ $(function(){
+ 	$(window).scroll(function(){
+ 		if($('body').scrollTop() > 100){
+ 		}
+ 	})
+ 	$(".top .banner .dropdown-content a").click(function(){
+ 		layhref = $(this).attr('href');
+ 		console.log(layhref);
+
+
+ 	  	$('body,html').animate({scrollTop:$(layhref).offset().top});
+ 	  	return false ; 
+ 	  
+ 	 })
+})  
+ 
