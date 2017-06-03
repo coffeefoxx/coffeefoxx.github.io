81ab6f0de42f7b390b233ab1d595705cc7fc958f
coffeefoxx <coffeefox.98@gmail.com>
2017-06-03 08:53:30 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 ﻿ $(function(){
  	// check vi tri 
- 	vitricuawelcome = $('.welcome').offset().top;   // tinh vi tri cua thang div welcome
+ 	// vitricuawelcome = $('.welcome').offset().top;   // tinh vi tri cua thang div welcome
  	 
 // ko tinh vi tri thi cung ko sao, dung luon 750 
 
 
  	$(window).scroll(function(event) { // khi cuon chuot thi goi ham 
 // $('body').scrollTop() --> tinh ra vi tri hien tai cua man hinh
- 		 if($('body').scrollTop() > vitricuawelcome) {  // so sanh vi tri hien tai voi welcome
+ 		 if($('body').scrollTop() > 150) {  // so sanh vi tri hien tai voi welcome
  		 	$('.navbar-brand.logo img').attr('src', 'images/index/logo.png'); // doi logo
  		  
  		 	 $('.navbar-brand.logo img').addClass('vang'); // them hieu ung 
@@ -19,7 +19,15 @@
  		 	$('.menutren').removeClass('nentrang');
  		 	$('.navbar-brand.logo img').attr('src', 'images/index/logo2.png');// ve nhu cu 
  		 }
-
  	});
+ 	$(".top .banner .btn").click(function(){
+ 		layhref = $(this).attr('href');
+ 		console.log(layhref);
+
+
+ 	  	$('body,html').animate({scrollTop:$(layhref).offset().top});
+ 	  	return false ; 
+ 	  
+ 	 })
 })  
  
